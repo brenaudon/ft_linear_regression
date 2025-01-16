f93716d7f172eb70547ea7fd7eae4082e5c66be6
brenaudon <brenaudon9@gmail.com>
2025-01-16 14:26:50 +0100

    feat: autodoc

---
+++ scripts/pydoctor.sh
@@ -0,0 +1,9 @@
+PROJECT_NAME=ft_linear_regression
+
+pydoctor \
+    --project-name=${PROJECT_NAME} \
+    --project-version=0.1 \
+    --project-url=https://github.com/brenaudon/$%7BPROJECT_NAME%7D/ \
+    --html-base-url=https://brenaudon.github.io/$%7BPROJECT_NAME%7D/ \
+    --docformat=numpy \
+    *.py
